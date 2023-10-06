a77578e23035d4d5711b895610eee0f50399f2d4
Nikita Frolkin <fokin-klock@mail.ru>
2023-10-06 07:38:29 +0400

    add: list

--- include/container_pair.h
+++ include/container_pair.h
@@ -1,32 +1,28 @@
 #include <pair.h>
-#include <array.h>
 
 #include <string>
 #include <iostream>
+#include <list.h>
+#include <exception.h>
 
 template<class T_key, class T_value>
 class Container_Pair
 {
 
-Array< pair<T_key, T_value> > _data;
-t_size _count_size;
+using t_size = int;
+
+List< pair<T_key, T_value> > _data;
 
 public:
-    Container_Pair() = delete;
+    Container_Pair(): _data() {}
     Container_Pair(const Container_Pair& other) = delete;
-    Container_Pair(t_size size): _count_size(0), _data(size) {}
     ~Container_Pair() {}
     
     Container_Pair& operator=(const Container_Pair& other) = delete;
 
-    t_size count_size()
+    t_size size()
     {
-        return this->_count_size;
-    }
-    
-    t_size max_size()
-    {
-        return this->_data.size();
+        return _data.size();
     }
 
     T_value& operator[](const T_key& search)
@@ -36,17 +32,14 @@ public:
             if ( _data[i].key() == search )
                 return _data[i].value();
         }
-        throw std::runtime_error("Don't search your value");
+        throw mstd::exception("Don't search your value");
     }
 
     void add(const pair<T_key, T_value>& for_add)
     {
-        if (_count_size + 1 > _data.size())
-            throw std::runtime_error("Your size is over");
         if ( !verification(for_add) )
             throw std::runtime_error("Pointed key been");
-        _data[_count_size] = for_add;
-        ++_count_size;
+        _data.push_back(for_add);
     }
 
 private:
@@ -62,24 +55,6 @@ private:
     }
 
 public:
-    void rm(const T_key& search)
-    {
-        t_size save_index = -1;
-        for(t_size i = 0; i < _data.size(); ++i)
-        {
-            if ( _data[i].key() == search )
-            {
-                save_index = i;
-                break;
-            }
-        }
-        if (save_index == -1)
-            throw std::runtime_error("Your key is fail");
-        
-        for(t_size i = save_index; i < this->_count_work - 1; ++i)
-            swap(this->_data[i], this->_data[i + 1]);
-        --_count_size;
-    }
 
     std::ostream& print(std::ostream& stream)
     {
